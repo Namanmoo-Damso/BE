d384b797b728c1b473d5417d9e56a430d2d533c4
sangyeon kim <greyhairchooselife@gmail.com>
2025-12-30 00:36:37 +0900

    feat: reverse-proxy 웹서버 추가; Caddy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // NestJS 서버가 받는 모든 요청의 기본 path
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000); // docker-compose.yml에서 PORT 환경변수 주입
 }
 bootstrap();
