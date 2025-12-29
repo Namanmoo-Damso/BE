b6789055cf8c6e1ef4c0e3b786723b05511111b8
sangyeon kim <greyhairchooselife@gmail.com>
2025-12-30 00:11:33 +0900

    feat: docker-compose.yml 생성
    - 개발서버 시작: `cd BE && docker compose up`
    - 개발서버 종료: `cd BE && docker compose down`
    - 패키지 설치: 컨테이너 접속 후 `npm install <pkg>`

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000); // docker-compose.yml에서 PORT 환경변수 주입
 }
 bootstrap();
